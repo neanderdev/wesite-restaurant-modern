80aaa251221e0bad82d87c81c99a9a8da94615a9
Neander de Souza <neander.faria@hotmail.com>
2024-01-18 16:36:19 -0300

    Special Menu finalizado

--- src/components/Menuitem/index.tsx
+++ src/components/Menuitem/index.tsx
@@ -1,8 +1,36 @@
 import './styles.css';
 
-export function MenuItem() {
+interface MenuItemProps {
+  title: string;
+  price: string;
+  tags: string;
+}
+
+export function MenuItem({ title, price, tags }: MenuItemProps) {
   return (
-    <div>MenuItem</div>
+    <div className='app__menuitem'>
+      <div className='app__menuitem-head'>
+        <div className='app__menuitem-name'>
+          <p className='p__cormorant' style={{ color: '#DCCA87' }}>
+            {title}
+          </p>
+        </div>
+
+        <div className='app__menuitem-dash' />
+
+        <div className='app__menuitem-price'>
+          <p className='p__cormorant'>
+            {price}
+          </p>
+        </div>
+      </div>
+
+      <div className='app__menuitem-sub'>
+        <p className='p__opensans' style={{ color: '#AAAAAA' }}>
+          {tags}
+        </p>
+      </div>
+    </div>
   );
 }
 
